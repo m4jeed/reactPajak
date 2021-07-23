78d72c43ff0126d1d81fe2e1ed83af93e60c782b
m4jeed <majeedprogrammerweb@gmail.com>
2021-07-23 13:31:08 +0700

    Updates

---
+++ precache-manifest.787b1372de286cd9ae380f55817af2c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238458fbe3166aa34abfd27865e9989b",
+    "url": "/reactPajak/index.html"
+  },
+  {
+    "revision": "e7d2634d40e47a3f052f",
+    "url": "/reactPajak/static/css/main.f0f5b9d2.chunk.css"
+  },
+  {
+    "revision": "24d29dd24abcedb1173a",
+    "url": "/reactPajak/static/js/2.ab38da78.chunk.js"
+  },
+  {
+    "revision": "e7d2634d40e47a3f052f",
+    "url": "/reactPajak/static/js/main.9e915d48.chunk.js"
+  },
+  {
+    "revision": "c691c7bb6c07cc4ca325",
+    "url": "/reactPajak/static/js/runtime-main.91b676b6.js"
+  }
+]);
